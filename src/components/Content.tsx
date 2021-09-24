9be3fa56be98c3a245a114116b5ea61bb2259264
netojso <neto.devx@gmail.com>
2021-09-24 11:27:38 -0300

    challenge finished

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import { MovieCard } from "./MovieCard";
 
 interface ContentProps {
@@ -19,7 +20,8 @@ interface ContentProps {
   }>;
 }
 
-export function Content({ selectedGenre, movies }: ContentProps) {
+function ContentComponent({ selectedGenre, movies }: ContentProps) {
+
   return (
     <div className="container">
       <header>
@@ -36,3 +38,7 @@ export function Content({ selectedGenre, movies }: ContentProps) {
     </div>
   )
 }
+
+export const Content = memo(ContentComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps, nextProps);
+})
